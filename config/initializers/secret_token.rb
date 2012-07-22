8e105118aeb9c58229a45ee1df07523aede7db5a
danil <danil@Home.(none)>
2012-07-23 01:08:02 +0600

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbakpressTask::Application.config.secret_token = 'b08af7cead2a27914428a80e62ee008263f07a9a1e4382c0a461022b12f6a05d63d2991af294c9e0fb51f6b554802467864f5a5dda4e6cc757dd4753ac3a07c0'
